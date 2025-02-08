511cf46ee3bf167dfd6def579272b81ad1ed1816
Ali <87234097+MianajiAli@users.noreply.github.com>
2025-02-08 18:11:08 +0330

    fix register page

--- front/src/app/auth/register/register.component.ts
+++ front/src/app/auth/register/register.component.ts
@@ -15,19 +15,14 @@ export class RegisterComponent {
     name: '',
     phone: '',
     password: '',
-    confirmPassword: ''
+
   };
-  passwordMismatch: boolean = false;
 
   constructor(private authService: AuthService, private router: Router) { }
 
   // This method handles the registration process
   onRegister() {
-    if (this.formData.password !== this.formData.confirmPassword) {
-      this.passwordMismatch = true;
-      return;
-    }
-    this.passwordMismatch = false;
+
 
     // Call the register method from AuthService
     this.authService.register(this.formData.name, this.formData.phone, this.formData.password)
