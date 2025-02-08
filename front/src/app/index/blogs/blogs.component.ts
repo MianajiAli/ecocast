fcf5af4d989d72827dd06f1882dec4dfe4db26a7
Ali <87234097+MianajiAli@users.noreply.github.com>
2025-02-08 18:43:33 +0330

    add blog page

--- front/src/app/index/blogs/blogs.component.ts
+++ front/src/app/index/blogs/blogs.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-blogs',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './blogs.component.html',
   styleUrl: './blogs.component.css'
 })
 export class BlogsComponent {
-
+  blogs = [
+    {
+      id: 1,
+      title: 'Understanding Angular',
+      excerpt: 'A beginner\'s guide to getting started with Angular.',
+      image: 'https://via.placeholder.com/400x200',
+    },
+    {
+      id: 2,
+      title: 'Tailwind CSS in Angular',
+      excerpt: 'How to integrate Tailwind CSS into your Angular project.',
+      image: 'https://via.placeholder.com/400x200',
+    },
+    {
+      id: 3,
+      title: 'Advanced JavaScript Techniques',
+      excerpt: 'Dive deeper into JavaScript with advanced techniques.',
+      image: 'https://via.placeholder.com/400x200',
+    }
+  ];
 }
