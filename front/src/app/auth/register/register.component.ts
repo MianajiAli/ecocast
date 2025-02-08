1bc3a651c87b7f234f02b352fb6cbf73222d214c
Ali <87234097+MianajiAli@users.noreply.github.com>
2025-02-08 17:27:01 +0330

    fix register page

--- front/src/app/auth/register/register.component.ts
+++ front/src/app/auth/register/register.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service'; // Import your AuthService
 import { Router } from '@angular/router'; // Import Router for navigation
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
+  imports: [FormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
