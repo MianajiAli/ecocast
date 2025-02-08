fc901eee39b3868a522c5d74fe4bf9e30d6fbe7e
Ali <87234097+MianajiAli@users.noreply.github.com>
2025-02-08 15:51:47 +0330

    Add login page

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,5 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: 'auth', loadChildren: () => import('./auth/auth.routes').then(m => m.routes) }
+
+    {
+        path: '',
+        loadChildren: () => import('./index/index.routes').then(m => m.routes)
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.routes').then(m => m.routes)
+    }
+
 ];
