0b50fad2aec2a012824a32a77c711b9e66c965f8
Ali <87234097+MianajiAli@users.noreply.github.com>
2025-02-10 02:47:46 +0330

    Revert "install passport"
    This reverts commit 23658ac8d63770ecfbe321aa61e6b94a23672416.

--- back/app/Providers/AppServiceProvider.php
+++ back/app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-use Laravel\Passport\Passport;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -22,9 +20,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        Passport::loadKeysFrom(__DIR__ . '/../secrets/oauth');
-        Passport::tokensExpireIn(now()->addDays(7));
-        Passport::refreshTokensExpireIn(now()->addDays(30));
-        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
     }
 }
