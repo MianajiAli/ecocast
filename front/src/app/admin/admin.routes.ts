de7297c6c7f5f69f71e3e56125b50e1a1ab1093a
Ali <87234097+MianajiAli@users.noreply.github.com>
2025-02-10 04:49:31 +0330

    add add blog html

---
+++ front/src/app/admin/admin.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+
+export const routes: Routes = [
+
+    {
+        path: 'blog',
+        component: BlogComponent
+    },
+
+];
--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -9,6 +9,9 @@ export const routes: Routes = [
     {
         path: 'auth',
         loadChildren: () => import('./auth/auth.routes').then(m => m.routes)
+    },
+    {
+        path: 'panel',
+        loadChildren: () => import('./admin/admin.routes').then(m => m.routes)
     }
-
 ];
