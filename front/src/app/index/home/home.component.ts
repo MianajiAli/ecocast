0de99aabb777e908e04526e5fdd851cdee3e44c6
Ali <87234097+MianajiAli@users.noreply.github.com>
2025-02-11 01:37:35 +0330

    Add font and improve home page

--- front/src/app/index/home/home.component.ts
+++ front/src/app/index/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BlogsComponent } from '../blogs/blogs.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [BlogsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
